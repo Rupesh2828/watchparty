540d9fa06b3e27327781cfde76077d05569a24ab
Rupesh Jadhav <rupesh.jadhav7700@gmail.com>
2024-11-15 18:59:40 +0530

    added more api routes

--- src/app/api/users/route.ts
+++ src/app/api/users/route.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from '@prisma/client';
+import { useParams } from 'next/navigation';
 import { NextResponse } from 'next/server';
+import bcrypt from "bcrypt";
 const db = new PrismaClient()
 
 
@@ -10,16 +12,18 @@ export const POST = async (req: Request, res: Response) => {
 
         if (!body) {
             return NextResponse.json(
-              { error: 'Request body is missing or invalid.' },
-              { status: 400 }
+                { error: 'Request body is missing or invalid.' },
+                { status: 400 }
             );
-          }
+        }
+
+        const hashedPassword = await bcrypt.hash(body.password, 10)
 
         const user = await db.user.create({
             data: {
                 username: body.username,
-                email:body.email,
-                password:body.password
+                email: body.email,
+                password: hashedPassword
             }
         })
 
@@ -30,5 +34,48 @@ export const POST = async (req: Request, res: Response) => {
         return NextResponse.json({ error: 'Failed to create user' }, { status: 500 });
 
     }
+
+}
+
+export const GET = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = await req.json()
+        // const id = useParams()
+    
+        if (!email || !password) {
+            return NextResponse.json(
+                { error: 'Email and Password is required' },
+                { status: 400 }
+            );
+        }
+    
+        const existingUser = await db.user.findUnique({
+            where: {email}
+        })
+    
+        if (!existingUser || !existingUser.password) {
+            return NextResponse.json(
+                { error: "User does not exist or invalid credentials." },
+                { status: 401 }
+            )
+        }
+    
+        const isPasswordValid = await bcrypt.compare(password, existingUser.password)
+    
+        if (!isPasswordValid) {
+            return NextResponse.json(
+                { error: 'Password is incorrect' },
+                { status: 401 }
+            );
+        }
+
+        return NextResponse.json({ message: 'Login successful', user: existingUser }, { status: 200 });
+
+    } catch (error) {
+        console.error('Error during login:', error);
+        return NextResponse.json({ error: 'Failed to login' }, { status: 500 });
+        
+    }
+
 
 }
